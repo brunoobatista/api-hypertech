7bbc7ed1fcf4b1a2eb730d97214f72172c9de353
brunoobatista <brunorafael.o@hotmail.com>
2019-06-11 23:29:57 -0300

    Ajuste em arquivos

--- src/main/java/com/rancotech/tendtudo/TendtudoApplication.java
+++ src/main/java/com/rancotech/tendtudo/TendtudoApplication.java
@@ -3,18 +3,20 @@ package com.rancotech.tendtudo;
 import com.rancotech.tendtudo.config.property.TendtudoApiProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TendtudoApiProperty.class)
 public class TendtudoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TendtudoApplication.class, args);
 	}
 
-	@Bean
+/*	@Bean
 	public TendtudoApiProperty tendtudoApiProperty() {
 		return new TendtudoApiProperty();
-	}
+	}*/
 
 }
